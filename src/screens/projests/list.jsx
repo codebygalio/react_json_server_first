9551bea282dbc081ffe6e94453260a360f9de84d
galio <galio@gmail.com>
2024-01-21 18:03:36 +0800

    done

---
+++ src/screens/projests/list.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const List = ({list, users}) => {
+    // console.log('list=', list)
+    // console.log('users=', users)
+    return <table>
+        <thead>
+            <tr>
+                <td>名称</td>
+                <td>负责人</td>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                list.map(project => <tr key={project.id}>
+                    <td>{project.name}</td>
+                    <td>{users.find(user => user.id === project.personId)?.name || '未知'}</td>
+                </tr>)
+            }
+        </tbody>
+    </table>
+}
